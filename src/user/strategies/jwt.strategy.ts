953fe0ae9d99b97a80d138f41b9e032d179a64c3
Sruthy <sruthyml328@gmail.com>
2021-05-04 13:48:06 +0530

    extract cookies from jwt

--- src/user/strategies/jwt.strategy.ts
+++ src/user/strategies/jwt.strategy.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
 import { jwtConstants } from '../../config/constants';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([(request: Request) => {
+        return request?.cookies?.Authentication;
+      }]),
       ignoreExpiration: false,
       secretOrKey: jwtConstants.secret,
     });
