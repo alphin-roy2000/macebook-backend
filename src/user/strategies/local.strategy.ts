75c454eb1d72d70cc1dd03533c3db74b88a46fce
Sruthy <sruthyml328@gmail.com>
2021-05-04 13:48:35 +0530

    add local strategy

---
+++ src/user/strategies/local.strategy.ts
@@ -0,0 +1,17 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { UserService } from '../user.service';
+import User from '../entities/user.entity';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: UserService) {
+    super({
+      usernameField: 'email'
+    });
+  }
+  async validate(email: string, password: string): Promise<any> {
+    return this.authService.validateUser(email, password);
+  }
+}
