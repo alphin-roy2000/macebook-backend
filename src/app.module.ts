865deef6c8dbd493a62e448e7019518035cf4829
AneezMuhammed <aneezmuhammed01@gmail.com>
2021-05-07 20:39:28 +0530

    Job module and entity addded

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './config/dbConfig';
 import { UserModule } from './user/user.module';
+import { JobsModule } from './jobs/jobs.module';
 
 
 
@@ -8,6 +9,7 @@ import { UserModule } from './user/user.module';
   imports: [
     ConfigModule.forRoot(),
     UserModule,
+    JobsModule,
   ],
   controllers: [],
   providers: [],
